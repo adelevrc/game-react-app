f2e96ecda8728e7aa40aed57045a6f27fe51217e
adelevrc <adele.vercaygne@gmail.com>
2021-04-20 11:45:41 +0200

    fetch game detail

--- src/api.js
+++ src/api.js
@@ -33,4 +33,6 @@ const new_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&orderin
 export const popularGamesURL = () => `${base_url}${popular_games}`
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`
 export const newGamesURL = () => `${base_url}${new_games}`
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}?key=${API_KEY}`
 
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots?key=${API_KEY}`
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,17 @@
 import React from 'react'; 
+import {useDispatch} from 'react-redux'; 
+import {loadDetail} from '../actions/detailAction'; 
 import styled from 'styled-components'; 
 import {motion} from 'framer-motion'; 
 
 const Game = ({ name, image, released, id }) => {
+
+    const dispatch = useDispatch();
+    const loadDetailHandler = () => {
+        dispatch(loadDetail(id))
+    }
     return(
-        <StyledGame>
+        <StyledGame onClick={loadDetailHandler}>
             <h3> {name} </h3>
             <p> {released} </p>
             <img src={image} alt={name} />
