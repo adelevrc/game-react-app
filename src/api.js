eee3bfc3367d67de5850e688903a2674b48aa20d
adelevrc <adele.vercaygne@gmail.com>
2021-04-20 09:58:24 +0200

    setting redux and dispatch - fecthing new popular and upcoming games

--- src/api.js
+++ src/api.js
@@ -24,8 +24,13 @@ const currentDay = getCurrentDay();
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`
+const API_KEY = '79b4d9a1031b474897020b2ebc9dd0e9'; 
 
-const popular_games = `games?dates=${lastYear},${currentDate}$ordering=-rating&page_size=10`; 
+const popular_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`; 
+const upcoming_games=`games?key=${API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`; 
+const new_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`
 
 export const popularGamesURL = () => `${base_url}${popular_games}`
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`
+export const newGamesURL = () => `${base_url}${new_games}`
 
